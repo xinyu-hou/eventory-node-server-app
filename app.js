006949e1cf213a8fe9b9261d247c6214c9ee36f8
xinyu-hou <xh4eu@virginia.edu>
2023-03-30 19:27:22 -0400

    xh - Interact with Ticketmaster API

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import bodyParser from "body-parser";
 import TestController from "./controllers/test-controller.js";
+import TicketmasterController from "./controllers/ticketmaster/ticketmaster-controller.js";
 
 const app = express();
+app.use(bodyParser.json());
 TestController(app);
+TicketmasterController(app);
 app.listen(4000);
---
+++ controllers/ticketmaster/ticketmaster-controller.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { config } from 'dotenv';
+config();
+
+const ticketmasterAPI = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+const ticketmasterAPIKey = process.env.TICKETMASTER_API_KEY;
+
+const TicketmasterController = (app) => {
+    app.get('/events', findEventsInMA);
+};
+
+const findEventsInMA = (req, res) => {
+    const params = {
+        apikey: ticketmasterAPIKey,
+        stateCode: 'MA',
+        size: req.query.size // max size value is 200
+    };
+    axios.get(ticketmasterAPI, {params})
+        .then(response => {
+            const events = response.data._embedded.events;
+            const eventsNameArray = [];
+            events.map(event => {
+                let eventItem = {
+                    "_id": event.id,
+                    "name": event.name,
+                    "linkToBuy": event.url,
+                    "date": event.dates.start.localDate,
+                    "time": event.dates.start.localTime,
+                    "segment": event.classifications[0].segment.name,
+                    "genre": event.classifications[0].genre.name,
+                    "subgenre": event.classifications[0].subGenre.name,
+                    "venueName": event._embedded.venues[0].name,
+                    "venueCity": event._embedded.venues[0].city.name,
+                    "venuePostalCode": event._embedded.venues[0].postalCode,
+                    "venueAddress": event._embedded.venues[0].address,
+                };
+                eventsNameArray.push(eventItem);
+            })
+            res.json(eventsNameArray);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).send('Error retrieving events. Contact developers for help.');
+        });
+}
+
+export default TicketmasterController;
